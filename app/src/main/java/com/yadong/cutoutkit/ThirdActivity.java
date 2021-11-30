4465d129d8d35785856a631886ff29d2f11a9205
houyadong <houyadong1@gome.com.cn>
2021-11-30 16:28:14 +0800

    Initial commit

---
+++ app/src/main/java/com/yadong/cutoutkit/ThirdActivity.java
@@ -0,0 +1,73 @@
+package com.yadong.cutoutkit;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.cutout.kit.CutoutScreenManager;
+import com.cutout.kit.StatusBarUtil;
+import com.navigationbar.kit.NavigationBarKit;
+
+public class ThirdActivity extends BaseActivity {
+
+    private NavigationBarKit mNavigationBarKit;
+    private LinearLayout mLinearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        mLinearLayout = findViewById(R.id.ll_container);
+        mNavigationBarKit = findViewById(R.id.nav_bar);
+        initNavigationBarKit();
+        setScreen();
+    }
+
+    private void initNavigationBarKit() {
+        mNavigationBarKit.setTitle("首页");
+        Button leftBtn1 = mNavigationBarKit.addLeftTextButton(getString(R.string.favorite), R.integer.id_left_btn1);
+        leftBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ThirdActivity.this, "左侧按钮", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button rightBtn1 = mNavigationBarKit.addRightTextButton(getString(R.string.share), R.integer.id_right_btn1);
+        rightBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ThirdActivity.this, "右侧按钮1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button rightBtn2 = mNavigationBarKit.addRightTextButton("分享", R.integer.id_right_btn2);
+        rightBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ThirdActivity.this, "右侧按钮2", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setScreen() {
+        int statusBarHeight = CutoutScreenManager.getInstance().getStatusBarHeight();
+        mLinearLayout.setPadding(0, statusBarHeight, 0, 0);
+        mLinearLayout.setBackgroundColor(Color.WHITE);
+    }
+
+    @Override
+    protected void setCutoutScreen(boolean isFullScreen) {
+        super.setCutoutScreen(true);
+    }
+
+    @Override
+    protected void setStatusBar() {
+        StatusBarUtil.setTransparent(this);
+        StatusBarUtil.setLightMode(this);
+    }
+}
