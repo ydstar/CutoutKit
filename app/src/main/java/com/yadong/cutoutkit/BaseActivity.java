9e7554f620013c2559c59973753773c5258f8569
houyadong <houyadong1@gome.com.cn>
2022-01-07 16:10:26 +0800

    整理代码

--- app/src/main/java/com/yadong/cutoutkit/BaseActivity.java
+++ app/src/main/java/com/yadong/cutoutkit/BaseActivity.java
@@ -16,23 +16,18 @@ import com.cutout.kit.immersionbar.ImmersionBar;
  */
 public class BaseActivity extends AppCompatActivity {
 
+    private CutoutScreenManager mInstance;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        setCutoutScreen(false);
-        setStatusBar();
         super.onCreate(savedInstanceState);
+        setStatusBar();
     }
 
-    /**
-     * 设置刘海屏,默认适配刘海屏和普通屏幕(子类可以重写该方法)
-     *
-     * @param isFullScreen 是否全屏展示(页面填充到状态栏区域) true:是  false:不是
-     *                     可以参考如下链接
-     *
-     */
-    protected void setCutoutScreen(boolean isFullScreen) {
-        CutoutScreenManager instance = CutoutScreenManager.getInstance();
-        instance.init(this, isFullScreen);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setCutoutScreen(false);
     }
 
     /**
@@ -44,4 +39,19 @@ public class BaseActivity extends AppCompatActivity {
                 .statusBarDarkFont(true)//状态栏字体为黑色
                 .init();
     }
+
+    /**
+     * 设置刘海屏,默认适配刘海屏和普通屏幕(子类可以重写该方法)
+     *
+     * @param isFullScreen 是否全屏展示(页面填充到状态栏区域) true:是  false:不是
+     *                     可以参考如下链接
+     */
+    protected void setCutoutScreen(boolean isFullScreen) {
+        if (mInstance == null) {
+            mInstance = CutoutScreenManager.getInstance();
+            mInstance.init(this, isFullScreen);
+        }
+    }
+
+
 }
